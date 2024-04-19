7a7e91e02cdef1aff2d1ed58b8d967409d0c92d3
alex-mateias <mateias.ionut.x6h@student.ucv.ro>
2024-04-19 13:19:13 +0300

    Implemented Repository.

--- tsk.DataAccess/Repository.cs
+++ tsk.DataAccess/Repository.cs
@@ -1,29 +1,45 @@
-﻿using tsk.Domain.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using tsk.Domain.Interfaces;
 
 public class Repository<T> : IRepository<T> where T : class
 {
-    public Task<T> AddAsync(T entity)
+    private readonly DbContext _context;
+
+    public Repository(DbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
     }
 
-    public Task DeleteAsync(Guid id)
+    public async Task<T> AddAsync(T entity)
     {
-        throw new NotImplementedException();
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return entity;
     }
 
-    public Task<IEnumerable<T>> GetAllAsync()
+    public async Task DeleteAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var entity = await _context.Set<T>().FindAsync(id);
+        if (entity == null)
+            throw new ArgumentException("Entity not found");
+
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
     }
 
-    public Task<T> GetAsync(Guid id)
+    public async Task<IEnumerable<T>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        return await _context.Set<T>().ToListAsync();
     }
 
-    public Task UpdateAsync(T entity)
+    public async Task<T> GetAsync(Guid id)
     {
-        throw new NotImplementedException();
+        return await _context.Set<T>().FindAsync(id) ?? throw new ArgumentException("Enriry not found");
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
     }
 }
