fd06edcf47be2a3f41f48e8973280e1eb28316b7
alex-mateias <mateias.ionut.x6h@student.ucv.ro>
2024-04-19 12:12:58 +0300

    Added Autofac.

---
+++ tsk.WebApi/AutofacModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using tsk.Domain.Interfaces;
+using tsk.Service;
+using tsk.Domain.Models;
+using tsk.DataAccess;
+
+public class AutofacModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<Service<TaskItem>>().As<IService<TaskItem>>();
+        builder.RegisterType<Repository<TaskItem>>().As<IRepository<TaskItem>>();
+
+        builder.RegisterType<UserService>().As<IUserService>();
+        builder.RegisterType<UserRepository>().As<IUserRepository>();
+    }
+}
--- tsk.WebApi/Program.cs
+++ tsk.WebApi/Program.cs
@@ -1,9 +1,16 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new AutofacModule());
+});
 
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
